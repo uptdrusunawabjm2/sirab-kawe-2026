a20f6f676cf6d05f8bd2a8e2967c121453247e3b
uptdrusunawabjm2 <uptd.rusunawa.bjm2@gmail.com>
2026-01-20 02:41:44 +0800

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,20 +1,16 @@
 export default async function handler(req, res) {
-  const { action, unit } = req.query;
+  const { action, key } = req.query;
+
   const GAS_URL = "https://script.google.com/macros/s/AKfycbzjABRVgRaoMj3Y1gCU_DRJpnZIvkGwxRRxmPW6buV1POsQ87GQCLAeEBG5j9jJZux7iQ/exec";
 
+  let url = GAS_URL + "?action=" + action;
+  if(key) url += "&key=" + key;
+
   try {
-    const url = `${GAS_URL}?action=${action||"summary"}${unit ? "&unit="+unit : ""}`;
     const r = await fetch(url);
     const j = await r.json();
     res.status(200).json(j);
   } catch (e) {
-    res.status(500).json({status:false, message:"API Error"});
+    res.status(500).json({status:false,message:e.toString()});
   }
 }
-
-
-
-
-
-
-
