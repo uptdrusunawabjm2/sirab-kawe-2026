141d396de2392b1ac7846cfb04b59ea7036874bc
uptdrusunawabjm2 <uptd.rusunawa.bjm2@gmail.com>
2026-01-21 00:38:15 +0800

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,17 +1,21 @@
 export default async function handler(req, res) {
-  const { action, key } = req.query;
+  try {
+    const action = String(req.query.action || "public");
+    const key = String(req.query.key || "");
 
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbzjABRVgRaoMj3Y1gCU_DRJpnZIvkGwxRRxmPW6buV1POsQ87GQCLAeEBG5j9jJZux7iQ/exec";
+    const GAS_URL = "https://script.google.com/macros/s/AKfycbzjABRVgRaoMj3Y1gCU_DRJpnZIvkGwxRRxmPW6buV1POsQ87GQCLAeEBG5j9jJZux7iQ/exec";
 
-  let url = GAS_URL + "?action=" + action;
-  if(key) url += "&key=" + key;
+    let url = GAS_URL + "?action=" + action;
+    if (key) url += "&key=" + key;
 
-  try{
     const r = await fetch(url);
-    const j = await r.json();
-    res.status(200).json(j);
-  }catch(e){
-    res.status(500).json({status:false,message:e.toString()});
+    const data = await r.json();
+
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({
+      status: false,
+      message: e.toString()
+    });
   }
 }
-
