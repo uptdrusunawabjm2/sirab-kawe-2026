e9b1e06c9c5e1efac1e6a7a730ab7d3991811492
uptdrusunawabjm2 <uptd.rusunawa.bjm2@gmail.com>
2026-01-30 16:10:05 +0800

    Add files via upload

---
+++ api/pdam.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res){
+  try{
+    const action = String(req.query.action || "summary");
+
+    const GAS_PDAM_URL =
+      "https://script.google.com/macros/s/AKfycbwXA1ikAoEsxJgvGVF9KD823rOw2TKokLe3Qyxw5JYl9iSGFGpS1zGCMsdI8bCg1JU0og/exec";
+
+    const url = GAS_PDAM_URL + "?action=" + action;
+    const r = await fetch(url);
+    const data = await r.json();
+
+    res.status(200).json(data);
+  }catch(e){
+    res.status(500).json({status:false, message:e.toString()});
+  }
+}
